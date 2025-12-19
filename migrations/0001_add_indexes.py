6b11a4ee3d161886ea9acdde54ddf2834918e4c9
Tung Nguyen <nvntung@gmail.com>
2025-12-19 18:28:48 +0000

    feat: rewrite the first migration script
    This script is used to create an index on the hash_md5 field

--- migrations/0001_add_indexes.py
+++ migrations/0001_add_indexes.py
@@ -1,14 +1,14 @@
 # migrations/0001_add_indexes.py
 
 version = "0001_add_indexes"
-description = "Create indexes on users collection"
+description = "Create indexes on photos collection"
 
 
 def upgrade(db, logger, session):
-    logger.info("Creating index on email")
-    # db.users.create_index("email", unique=True, session=session)
+    logger.info("Creating index on hash_md5")
+    db.photos.create_index("hash_md5", unique=True, session=session)
 
 
 def downgrade(db, logger, session):
-    logger.info("Dropping index on email")
-    # db.users.drop_index("email_1", session=session)
+    logger.info("Downgrading from hash_md5 not supported for this version")
+    raise NotImplementedError("Irreversible migration")
