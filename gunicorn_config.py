3fb09777e73dc166033eb23489a123a8f4ffad61
Tung Nguyen <nvntung@gmail.com>
2025-12-21 16:04:14 +0000

    chore: modify the start script

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,17 +1,23 @@
+import multiprocessing
 import os
 
 
-
-workers = int(os.environ.get('GUNICORN_PROCESSES', '4'))
+workers = multiprocessing.cpu_count() * 2 + 1
+# workers = int(os.environ.get('GUNICORN_PROCESSES', '4'))
 
 threads = int(os.environ.get('GUNICORN_THREADS', '8'))
 
 # timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
 
-bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:5000')
+bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:5500')
 
-
+keyfile = 'key.pem'
+certfile = 'cert.pem'
 
 forwarded_allow_ips = '*'
 
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+
+# Logging
+accesslog = 'logs/access.log'
+errorlog = 'logs/error.log'
--- start.sh
+++ start.sh
@@ -4,5 +4,11 @@ export UPLOAD_FOLDER=/Users/tnguyen/ownCloud/MyBusiness/data/photo-manager
 export UPLOAD_FOLDER=/home/tnguyen/Documents/MyBusiness/PhotoManager/photodb
 #export UPLOAD_FOLDER=/home/tnguyen/Documents/MyBusiness/PhotoManager/mygallery
 #flask run --host=0.0.0.0 --port=5500 --debug >> logs/run.log
-python app.py
-#gunicorn --config gunicorn_config.py app:app
+#python app.py
+gunicorn --config gunicorn_config.py app:app
+# OR
+#gunicorn --bind 0.0.0.0:5500 \
+#  --workers 17 \
+#  --certfile=cert.pem \
+#  --keyfile=key.pem \
+#  app:app
