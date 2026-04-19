d442f55ce34ca776a0176f1c3858c431a0600e24
Tung Nguyen <nvntung@gmail.com>
2026-04-19 09:22:30 +0100

    PM-17 chore: revise .env files for different environments

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 export FLASK_APP=app
 export FLASK_ENV=development
-export UPLOAD_FOLDER=/Users/tnguyen/ownCloud/MyBusiness/data/PM/photodb-files
-#export UPLOAD_FOLDER=/home/tnguyen/Documents/MyBusiness/PhotoManager/photodb
-#export UPLOAD_FOLDER=/home/tnguyen/Documents/MyBusiness/PhotoManager/mygallery
+# UPLOAD_FOLDER is set in .env and loaded by python-dotenv at startup
 #flask run --host=0.0.0.0 --port=5500 --debug >> logs/run.log
 #python app.py
 gunicorn app.app:app \
